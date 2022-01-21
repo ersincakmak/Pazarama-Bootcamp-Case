ebde73b63ee10e8d0fdd988c1b60ae69885d8e7e
ersincakmak <ckmrsn@gmail.com>
2022-01-22 02:09:22 +0300

    Dropzone test cases created

---
+++ src/components/DropZone/DropZone.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import DropZone from '.'
+
+describe('Dropzone Test', () => {
+  it('should render Dropzone component', () => {
+    const { baseElement } = render(<DropZone onDrop={jest.fn()} />)
+    expect(baseElement).toBeInTheDocument()
+  })
+})
