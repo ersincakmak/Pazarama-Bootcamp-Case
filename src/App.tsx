170f8edab2a908ce628cfa6c45e44bba3e6f72d6
ersincakmak <ckmrsn@gmail.com>
2022-01-21 11:33:33 +0300

    Setup react app with typescript, eslint and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="underline font-Nunito font-bold text-2xl text-slate-500">
+      Hello World
+    </div>
+  )
+}
+
+export default App
