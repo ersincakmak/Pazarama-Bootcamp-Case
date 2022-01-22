c912760cb5569f922933b4187bf8c2a888d6f973
ersincakmak <ckmrsn@gmail.com>
2022-01-22 12:23:23 +0300

    Create modal close button

--- src/components/ImagePreview/index.tsx
+++ src/components/ImagePreview/index.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react'
 import Modal from 'react-modal'
+import { FaTimes } from 'react-icons/fa'
 
 interface Props {
   file: File
@@ -21,6 +22,7 @@ const ImagePreview: React.FC<Props> = ({ file }) => {
   return (
     <>
       <div
+        data-testid="image-modal-text"
         onClick={openModal}
         className="border-2 border-stone-300 hover:border-stone-500 cursor-pointer transition w-max p-3 rounded-md flex items-center justify-center"
       >
@@ -32,10 +34,18 @@ const ImagePreview: React.FC<Props> = ({ file }) => {
         className="p-0 absolute top-0 left-0 right-0 bottom-0 max-w-full w-max m-auto h-full max-h-72 px-3"
         ariaHideApp={false}
       >
+        <button
+          type="button"
+          className="ml-auto text-xl absolute top-0 transform -translate-y-full right-0 p-2"
+          onClick={closeModal}
+        >
+          <FaTimes />
+        </button>
         <img
           src={URL.createObjectURL(file)}
           alt={file.name}
           className="w-full h-full object-contain"
+          data-testid="image-modal"
         />
       </Modal>
     </>
