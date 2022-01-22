d7d654dc97ceb5c25fc7b619bce3ac8f0e2272ed
ersincakmak <ckmrsn@gmail.com>
2022-01-23 01:51:43 +0300

    Create spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {
+  size: 'sm' | 'md' | 'xl'
+}
+
+const Spinner: React.FC<Props> = ({ size }) => {
+  const getSize = {
+    sm: 'w-5 h-5',
+    md: 'w-7 h-7',
+    xl: 'w-9 h-9',
+  }
+
+  return (
+    <span
+      className={`border-white border-2 rounded-full inline-block
+      animate-spin border-t-transparent ${getSize[size]}`}
+    />
+  )
+}
+
+export default Spinner
