6e82e271181df30534d612663d077c165b80756d
ersincakmak <ckmrsn@gmail.com>
2022-01-22 12:18:28 +0300

    Wrap test files with __test__ folder

--- src/components/DropZone/__test__/DropZone.test.tsx
+++ src/components/DropZone/__test__/DropZone.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import DropZone from '.'
+import DropZone from '..'
 
 describe('Dropzone Test', () => {
   it('should render Dropzone component', () => {
