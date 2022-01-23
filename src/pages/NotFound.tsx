69f385b634c7bb33bdfcf52e0341c4466fc9aae2
ersincakmak <ckmrsn@gmail.com>
2022-01-23 14:07:21 +0300

    Update Not Found style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,7 @@ const NotFound = () => (
   <div className="w-screen h-screen bg-orange-100 flex">
     <div className="flex flex-col items-center gap-3 m-auto w-max">
       <h1 className="text-5xl font-semibold">404 Not Found</h1>
-      <p>
+      <p className="text-center">
         For application inquiry go to{' '}
         <Link to="/basvuru-sorgula" className="text-blue-400 underline">
           Application Inquiry Page
