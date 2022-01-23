67317044235a15129ad527a01ee49d9166ea7f2a
ersincakmak <ckmrsn@gmail.com>
2022-01-23 11:31:50 +0300

    Update createApplication validation schema

--- src/validations/Application.ts
+++ src/validations/Application.ts
@@ -1,16 +1,20 @@
 import * as yup from 'yup'
 
 const createApplicationSchema = yup.object().shape({
-  firstName: yup.string().required().label('First Name'),
-  lastName: yup.string().required().label('Last Name'),
-  age: yup.number().required().label('Age'),
-  applicationReason: yup.string().required().label('Application Reason'),
+  firstName: yup.string().required().min(3).label('First Name'),
+  lastName: yup.string().required().min(3).label('Last Name'),
+  age: yup.number().required().positive().label('Age'),
+  applicationReason: yup
+    .string()
+    .min(10)
+    .required()
+    .label('Application Reason'),
   tcNo: yup
     .string()
     .required()
     .matches(/^[1-9]{1}[0-9]{9}[02468]{1}$/, 'Invalid Tc No')
     .label('Tc No'),
-  address: yup.string().required().label('Address'),
+  address: yup.string().required().min(5).label('Address'),
 })
 
 export default createApplicationSchema
