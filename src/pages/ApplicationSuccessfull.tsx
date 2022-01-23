f8edff815bd2c36aa2ac07002c4dda0040b99be3
ersincakmak <ckmrsn@gmail.com>
2022-01-23 14:40:13 +0300

    Use ApplicationDetail component in ApplicationSuccessfull page

--- src/pages/ApplicationSuccessfull.tsx
+++ src/pages/ApplicationSuccessfull.tsx
@@ -1,27 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { AiOutlineCheck } from 'react-icons/ai'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import ApplicationDetail from '../components/ApplicationDetail'
 import { useAppSelector } from '../redux/store'
-
-type Props = {
-  title: string
-}
-
-const Detail: React.FC<Props> = ({ title, children }) => (
-  <div className="w-full flex flex-col items-start md:flex-row md:items-center gap-2">
-    <p className="text-lg font-semibold">{title}:</p>
-    <p>{children}</p>
-  </div>
-)
 
 const ApplicationSuccessfull = () => {
   const { application } = useAppSelector((state) => state.application)
-  const navigate = useNavigate()
-  useEffect(() => {
-    if (!application) {
-      navigate('/basvuru-olustur')
-    }
-  }, [application])
+
+  if (!application) {
+    return <Navigate to="/basvuru-olustur" />
+  }
 
   return (
     <div className="w-screen h-screen overflow-auto flex flex-col gap-2 p-3 bg-orange-100">
@@ -51,18 +39,7 @@ const ApplicationSuccessfull = () => {
             </p>
           </div>
         </div>
-        <div className="p-3 rounded-xl border-2 border-slate-300 bg-white flex flex-col gap-3">
-          <p className="text-2xl font-bold text-center">Application Detail</p>
-          <Detail title="Status">{application?.status}</Detail>
-          <Detail title="First Name">{application?.firstName}</Detail>
-          <Detail title="Last Name">{application?.lastName}</Detail>
-          <Detail title="Age">{application?.age}</Detail>
-          <Detail title="Tc No">{application?.tcNo}</Detail>
-          <Detail title="Address">{application?.address}</Detail>
-          <Detail title="Application Reason">
-            {application?.applicationReason}
-          </Detail>
-        </div>
+        <ApplicationDetail application={application} />
       </div>
     </div>
   )
