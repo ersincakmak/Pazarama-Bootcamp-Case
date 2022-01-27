18c417f08b7af4a78a4fe885766c0d9e129d8e50
ersincakmak <ckmrsn@gmail.com>
2022-01-28 00:36:45 +0300

    Add more test case for AdminLogin

--- src/pages/AdminLogin/__tests__/AdminLogin.test.tsx
+++ src/pages/AdminLogin/__tests__/AdminLogin.test.tsx
@@ -1,20 +1,116 @@
-import { render } from '@testing-library/react'
+import {
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import AdminLogin from '..'
+import api from '../../../constants/axios'
 import store from '../../../redux/store'
 
-describe('AdminLogin Page Test', () => {
-  it('should render correctly', () => {
-    const { baseElement } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <AdminLogin />
-        </BrowserRouter>
-      </Provider>
-    )
+jest.mock('../../../constants/axios.ts')
+jest.mock('react-toastify')
 
+const mockedAxios = api as jest.Mocked<typeof api>
+const mockedToast = toast as jest.Mocked<typeof toast>
+
+const renderElement = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <AdminLogin />
+    </BrowserRouter>
+  </Provider>
+)
+
+describe('AdminLogin Page Test', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should render correctly', () => {
+    const { baseElement } = render(renderElement())
     expect(baseElement).toBeInTheDocument()
+  })
+
+  it('should navigate when login successfull', async () => {
+    mockedAxios.post.mockResolvedValue({
+      data: {
+        data: {
+          username: 'Test Username',
+          name: 'Test Name',
+        },
+      },
+    })
+
+    await act(async () => {
+      const { getByRole, getByLabelText, getByTestId } = render(renderElement())
+
+      fireEvent.change(
+        getByRole('textbox', {
+          name: /username/i,
+        }),
+        {
+          target: {
+            value: 'test',
+          },
+        }
+      )
+
+      fireEvent.change(getByLabelText(/password/i), {
+        target: {
+          value: 'test',
+        },
+      })
+
+      fireEvent.click(
+        getByRole('button', {
+          name: /login/i,
+        })
+      )
+
+      await waitFor(async () => expect(mockedAxios.post).toBeCalled())
+      await waitForElementToBeRemoved(getByTestId('spinner'))
+      expect(window.location.pathname).toBe('/admin/basvuru-listesi')
+    })
+  })
+
+  it('should navigate when login rejected', async () => {
+    mockedAxios.post.mockRejectedValue({})
+
+    await act(async () => {
+      const { getByRole, getByLabelText, getByTestId } = render(renderElement())
+
+      fireEvent.change(
+        getByRole('textbox', {
+          name: /username/i,
+        }),
+        {
+          target: {
+            value: 'test',
+          },
+        }
+      )
+
+      fireEvent.change(getByLabelText(/password/i), {
+        target: {
+          value: 'test',
+        },
+      })
+
+      fireEvent.click(
+        getByRole('button', {
+          name: /login/i,
+        })
+      )
+
+      await waitFor(() => expect(mockedAxios.post).toBeCalled())
+      await waitForElementToBeRemoved(getByTestId('spinner'))
+      await waitFor(() => expect(mockedToast.error).toBeCalled())
+    })
   })
 })
