f3ece73218608991bf9e850fef5c67230b476263
ersincakmak <ckmrsn@gmail.com>
2022-01-28 01:56:26 +0300

    Add more test case for AdminNav

--- src/components/AdminNav/__tests__/AdminNav.test.tsx
+++ src/components/AdminNav/__tests__/AdminNav.test.tsx
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import AdminNav from '..'
@@ -17,5 +18,17 @@ describe('AdminNav test', () => {
   it('should render correctly', async () => {
     const { baseElement } = render(renderElement())
     expect(baseElement).toBeInTheDocument()
+  })
+
+  it('should logout correctyl when click logout button', async () => {
+    await act(async () => {
+      const { getByRole } = render(renderElement())
+      await fireEvent.click(
+        getByRole('button', {
+          name: /logout/i,
+        })
+      )
+      expect(store.getState().admin.user).toBe(null)
+    })
   })
 })
