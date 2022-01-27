7cb70f5de671c8de507af1d2ff4976cff703ffd8
ersincakmak <ckmrsn@gmail.com>
2022-01-28 00:08:39 +0300

    Add more test case for AdminApplicationList

--- src/pages/AdminApplicationList/__tests__/AdminApplicationList.test.tsx
+++ src/pages/AdminApplicationList/__tests__/AdminApplicationList.test.tsx
@@ -1,20 +1,42 @@
 import { render } from '@testing-library/react'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import AdminApplicationList from '..'
+import api from '../../../constants/axios'
+import { ApplicationMock } from '../../../mocks/Application'
 import store from '../../../redux/store'
 
-describe('AdminApplicationList Page Test', () => {
-  it('should render correctly', () => {
-    const { baseElement } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <AdminApplicationList />
-        </BrowserRouter>
-      </Provider>
-    )
+jest.mock('../../../constants/axios.ts')
+const mockedAxios = api as jest.Mocked<typeof api>
 
-    expect(baseElement).toBeInTheDocument()
+const renderElement = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <AdminApplicationList />
+    </BrowserRouter>
+  </Provider>
+)
+
+describe('AdminApplicationList Page Test', () => {
+  it('should render correctly without data', async () => {
+    mockedAxios.get.mockRejectedValue({})
+    await act(async () => {
+      const { baseElement } = render(renderElement())
+      expect(baseElement).toBeInTheDocument()
+    })
+  })
+
+  it('should render correctly with data', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        data: [{ ...ApplicationMock }],
+      },
+    })
+    await act(async () => {
+      const { baseElement } = render(renderElement())
+      expect(baseElement).toBeInTheDocument()
+    })
   })
 })
