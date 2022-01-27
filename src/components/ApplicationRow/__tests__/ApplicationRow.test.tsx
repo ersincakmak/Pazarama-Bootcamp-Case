c75c195f364726375c81118ed9338b3fc1a201c7
ersincakmak <ckmrsn@gmail.com>
2022-01-28 02:17:04 +0300

    Change local mock data to global mock data

--- src/components/ApplicationRow/__tests__/ApplicationRow.test.tsx
+++ src/components/ApplicationRow/__tests__/ApplicationRow.test.tsx
@@ -2,28 +2,13 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ApplicationRow from '..'
-import { IApplication } from '../../../types/application'
-
-const mockApplication: IApplication = {
-  _id: '',
-  address: '',
-  age: 0,
-  answers: [],
-  applicationReason: '',
-  createdAt: '',
-  files: [],
-  firstName: '',
-  lastName: '',
-  status: 'waiting',
-  tcNo: '',
-  updatedAt: '',
-}
+import { ApplicationMock } from '../../../mocks/Application'
 
 describe('Dropzone Test', () => {
   it('should render Dropzone component', () => {
     const { baseElement } = render(
       <BrowserRouter>
-        <ApplicationRow application={mockApplication} />
+        <ApplicationRow application={ApplicationMock} />
       </BrowserRouter>
     )
     expect(baseElement).toBeInTheDocument()
